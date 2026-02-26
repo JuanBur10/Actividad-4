a5a7b9d06b7cdd6390ad5948600af9f6f08c4c60
JuanBur10 <juanjosebur12@gmail.com>
2026-02-25 19:27:00 -0500

    Primer commit - Taller Control Condicional

---
+++ Actividad/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        string resultado = CheckNumber(numero);
+        Console.WriteLine(resultado);
+    }
+
+    static string CheckNumber(int numero)
+    {
+        if (numero > 0)
+        {
+            return "El número es positivo";
+        }
+        else if (numero < 0)
+        {
+            return "El número es negativo";
+        }
+        else
+        {
+            return "El número es cero";
+        }
+    }
+}
